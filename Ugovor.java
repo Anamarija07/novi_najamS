eb512ba87d8a3755998f0baa00e65334ffe38e55
Anamarija07 <49204564+Anamarija07@users.noreply.github.com>
2019-06-29 13:20:51 +0200

    Add files via upload

---
+++ Ugovor.java
@@ -0,0 +1,42 @@
+package controller;
+
+import com.sun.prism.d3d.D3DTextureData;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Ugovor {
+    @FXML
+    TextField vIme;
+
+    @FXML
+    TextField vPrezime;
+
+    @FXML
+    TextField kIme;
+
+    @FXML
+    TextField kPrezime;
+
+    @FXML
+    TextField adresa;
+
+    @FXML
+    TextField mjesto;
+
+    @FXML
+    TextField brKvadrata;
+
+    @FXML
+    TextField brSoba;
+
+    @FXML
+    TextField cijena;
+
+    @FXML
+    TextArea opis;
+
+    @FXML
+    DatePicker datum;
+}
